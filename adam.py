67b94b6a71c25c55b6522edb0b27a7618d6e29cf
ke <ke.yao@elegine.ai>
2018-11-12 17:21:59 -0500

    added CV

--- adam.py
+++ adam.py
@@ -10,7 +10,8 @@ class Adam:
         self.beta_1 = .9
         self.beta_2 = .999
 
-        self.num_iterations = 20
+        self.num_iterations = 200
+        self.batch_size = 128
 
     def sigmoid(self, x):
         y = 1.0 / (1.0 + np.exp(-x))
@@ -21,39 +22,56 @@ class Adam:
         softmax = z_exp/(sum(z_exp))
         return softmax
 
+    def one_hot_encoding(self, y):
+        #m, n = y.shape
+        length = len(y)
+        categories = 10
+        one_hot = np.zeros((length, categories))
+        for i in range(length):
+            one_hot[i][y[i]] = 1
+
+        return one_hot
+
+
 
     def compute_grad(self, W, b, X, Y):
 
-        #print ("W:", W.shape)
-        #print ("X:", X.shape)
-
-        m = X.shape[1]
+        m = X.shape[0]
 
         logits = np.dot(W.T,X) + b
         O = self.softmax(logits)
+        Y = self.one_hot_encoding(Y).T
 
-        dw = -1.0/m*np.dot(X, (O-Y).T)
-        db = -1.0/m*np.sum(O-Y)
+        cost = -(1.0/m)*np.sum(Y*np.log(O) + (1.0-Y)*np.log(1.0-O))
+
+        dw = -(1.0/m)*np.dot(X, (O-Y).T)
+        db = -(1.0/m)*np.sum(O-Y)
 
         grads = {"dw":dw, "db":db}
 
-        return grads
+        return grads, cost
 
-    def optimize(self,W, b, X, Y):
+    def adam_optimize(self,W, b, X, Y):
+
+        X = X.T
         alpha = self.alpha
         beta_1 = self.beta_1
         beta_2 = self.beta_2
+        num_iterations = self.num_iterations
         momentum_w = 0
         momentum_b = 0
         v_w = 0
         v_b = 0
         eps = 1e-8
-        num_iterations = self.num_iterations
-
+        costs = []
         iter = 0
+
         while  iter < num_iterations:
             iter += 1
-            grads_w, grads_b = self.compute_grad(W,b,X,Y)['dw'], self.compute_grad(W,b,X,Y)['db']
+            grads, cost = self.compute_grad(W,b,X,Y)
+            grads_w = grads['dw']
+            grads_b = grads['db']
+            costs.append(cost)
 
             #Update biased first moment estimate
             momentum_w = beta_1*momentum_w + (1-beta_1)*grads_w
@@ -80,44 +98,103 @@ class Adam:
             b += db
 
         params = {"W":W, "b":b}
-        return params
+        return params, costs
+
+    def optimize(self, W, b, X, Y):
+        X = X.T
+        costs = []
+        iter = 0
+        while iter < self.num_iterations:
+            iter += 1
+            grads, cost = self.compute_grad(W, b, X, Y)
+            costs.append(cost)
+            dw = grads['dw']
+            db = grads['db']
+
+            W += self.alpha*dw
+            b += self.alpha*db
+
+
+        params = {"W":W, "b":b}
+
+        return params, costs
+
+
 
     def predict(self, W, b, X):
+        X = X.T
 
         logit = self.softmax(np.dot(W.T, X) + b)
-        #print(A.shape)
-
         y_pred = np.argmax(logit, axis=0)
-        #print (y_pred.shape)
+
 
         return y_pred
 
-    def model(self,X_train, Y_train, X_test, Y_test):
 
+
+    def model(self, X_train, Y_train, X_test, Y_test):
+        #TODO: add mini batches
         #initialize parameterss
-        W = np.zeros((784,10))
-        b = np.zeros((10,1))
+        W = np.random.rand(784,10)
+        b = np.random.rand(10,1)
 
         X_train = X_train.reshape(-1,784)
-        X_train = X_train.T
         X_test = X_test.reshape(-1,784)
-        X_test = X_test.T
 
-        params = self.optimize(W, b, X_train, Y_train)
+        X = X_train
+        Y = Y_train
+
+
+        index = len(X_train)//5
+
+        train_costs_list = []
+        val_costs_list = []
+
+        for i in range(5):
+            p = np.random.permutation(len(X))
+            X = X[p]
+            Y = Y[p]
+
+            X_val = X[:index,:]
+            Y_val = Y[:index]
+            X_train = X[index:,:]
+            Y_train = Y[index:]
+
+
+            params, train_costs = self.optimize(W, b, X_train, Y_train)
+            val_params, val_costs = self.optimize(W, b, X_val, Y_val)
+
+            print(train_costs)
+            print(val_costs)
+
+
+            train_costs_list.append(train_costs)
+            val_costs_list.append(val_costs)
+
+
+
+        train_costs = np.array(train_costs_list)
+        val_costs = np.array(val_costs_list)
+
+        train_costs = sum(train_costs)/10
+        print(train_costs)
+        val_costs = sum(val_costs)/10
+        print(val_costs)
         W = params['W']
-        print(sum(W))
         b = params['b']
 
         y_pred_train = self.predict(W,b,X_train)
         y_pred_test = self.predict(W,b,X_test)
 
+
         train_acc = sum(y_pred_train == Y_train) / len(Y_train)
         test_acc = sum(y_pred_test == Y_test) / len(Y_test)
+        val_acc = sum()
 
         print ("Train acc:", train_acc)
         print ("Test acc:", test_acc)
 
-        return test_acc
+        return train_costs, val_costs
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import tensorflow as tf
 from adam import Adam
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     Adam = Adam()
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    #x_train = x_train[:100,:,:]
+    #y_train = y_train[:100]
+    print(y_train.shape)
+    print(y_train[:10])
+    print(x_train.shape)
+    #print(Adam.one_hot_encoding(y_train))
     x_train, x_test = x_train / 255.0, x_test / 255.0
-    d = Adam.model(x_train, y_train, x_test, y_test)
+
+    train_costs,val_costs = Adam.model(x_train, y_train, x_test, y_test)
+
+    plt.plot(range(len(train_costs)), train_costs)
+    plt.show()
+
+    plt.plot(range(len(val_costs)), val_costs)
+    plt.show()
+
+
 

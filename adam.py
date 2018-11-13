3fab9a18070d9ef4baf34928627fbf6afdf61f4a
Dvangelion <yooork99@gmail.com>
2018-11-13 16:13:55 -0500

    Adam

--- adam.py
+++ adam.py
@@ -10,7 +10,7 @@ class Adam:
         self.beta_1 = .9
         self.beta_2 = .999
 
-        self.num_iterations = 200
+        self.num_iterations = 10
         self.batch_size = 128
 
     def sigmoid(self, x):
@@ -37,7 +37,6 @@ class Adam:
     def compute_grad(self, W, b, X, Y):
 
         m = X.shape[0]
-
         logits = np.dot(W.T,X) + b
         O = self.softmax(logits)
         Y = self.one_hot_encoding(Y).T
@@ -51,8 +50,7 @@ class Adam:
 
         return grads, cost
 
-    def adam_optimize(self,W, b, X, Y):
-
+    def optimize(self, W, b, X, Y, batch_size):
         X = X.T
         alpha = self.alpha
         beta_1 = self.beta_1
@@ -65,42 +63,61 @@ class Adam:
         eps = 1e-8
         costs = []
         iter = 0
-
-        while  iter < num_iterations:
+        total_batches = len(Y) // batch_size
+        while iter < num_iterations:
             iter += 1
-            grads, cost = self.compute_grad(W,b,X,Y)
-            grads_w = grads['dw']
-            grads_b = grads['db']
-            costs.append(cost)
+            i = 0
+            cost = 0
+            p = np.random.permutation(len(X.T))
+            X = X[:, p]
+            Y = Y[p]
 
-            #Update biased first moment estimate
-            momentum_w = beta_1*momentum_w + (1-beta_1)*grads_w
-            momentum_b = beta_1*momentum_b + (1-beta_1)*grads_b
+            while i < total_batches:
 
-            #Update biased second raw moment estimate
-            v_w = beta_2*v_w + (1-beta_2)*(grads_w*grads_w)
-            v_b = beta_2*v_b + (1-beta_2)*(grads_b*grads_b)
+                batch_X = X[:, i*batch_size:(i+1)*batch_size]
+                batch_Y = Y[i*batch_size:(i+1)*batch_size]
 
-            #Compute bias-corrected first moment estimate
-            m_hat_w = momentum_w/(1-beta_1**iter)
-            m_hat_b = momentum_b/(1-beta_1**iter)
+                i += 1
+                grads, batch_cost = self.compute_grad(W, b, batch_X, batch_Y)
+                grads_w = grads['dw']
+                grads_b = grads['db']
+                costs.append(batch_cost)
 
-            #Compute bias-corrected second raw moment estimate
-            v_hat_w = v_w/(1-beta_2**iter)
-            v_hat_b = v_b/(1-beta_2**iter)
+                alpha = alpha / np.sqrt(iter)
 
-            #Compute parameters to update
-            dW = alpha*m_hat_w/(np.sqrt(v_hat_w)+eps)
-            db = alpha*m_hat_b/(np.sqrt(v_hat_b)+eps)
+                #Update biased first moment estimate
+                momentum_w = beta_1*momentum_w + (1-beta_1)*grads_w
+                momentum_b = beta_1*momentum_b + (1-beta_1)*grads_b
 
-            #Update parameters
-            W += dW
-            b += db
+		#Update biased second raw moment estimate
+                v_w = beta_2*v_w + (1-beta_2)*(grads_w*grads_w)
+                v_b = beta_2*v_b + (1-beta_2)*(grads_b*grads_b)
 
+		#Compute bias-corrected first moment estimate
+                m_hat_w = momentum_w/(1-beta_1**iter)
+                m_hat_b = momentum_b/(1-beta_1**iter)
+
+                #Compute bias-corrected second raw moment estimate
+                v_hat_w = v_w/(1-beta_2**iter)
+                v_hat_b = v_b/(1-beta_2**iter)
+
+                #Compute parameters to update
+                dW = alpha*m_hat_w/(np.sqrt(v_hat_w)+eps)
+                db = alpha*m_hat_b/(np.sqrt(v_hat_b)+eps)
+
+                #Update parameters
+                W += dW
+                b += db
+
+            #costs.append(cost/total_batches)
+
+
+        costs = np.array(costs)
         params = {"W":W, "b":b}
+
         return params, costs
 
-    def optimize(self, W, b, X, Y):
+    def grad_optimize(self, W, b, X, Y):
         X = X.T
         costs = []
         iter = 0
@@ -131,7 +148,6 @@ class Adam:
         return y_pred
 
 
-
     def model(self, X_train, Y_train, X_test, Y_test):
         #TODO: add mini batches
         #initialize parameterss
@@ -143,55 +159,65 @@ class Adam:
 
         X = X_train
         Y = Y_train
-
-
-        index = len(X_train)//5
+        
+        #5-fold cross validation
+        index = len(X_train) // 5
+        
+        X_val = X[:index,:]
+        Y_val = Y[:index]
+        X_train = X[index:,:]
+        Y_train = Y[index:]
 
         train_costs_list = []
+        train_acc_list = []
         val_costs_list = []
+        val_acc_list = []
 
         for i in range(5):
-            p = np.random.permutation(len(X))
-            X = X[p]
-            Y = Y[p]
-
-            X_val = X[:index,:]
-            Y_val = Y[:index]
-            X_train = X[index:,:]
-            Y_train = Y[index:]
 
 
-            params, train_costs = self.optimize(W, b, X_train, Y_train)
-            val_params, val_costs = self.optimize(W, b, X_val, Y_val)
 
-            print(train_costs)
-            print(val_costs)
 
+
+            params, train_costs = self.optimize(W, b, X_train, Y_train, self.batch_size)
+            val_params, val_costs = self.optimize(W, b, X_val, Y_val, self.batch_size)
 
             train_costs_list.append(train_costs)
             val_costs_list.append(val_costs)
+
+            y_pred_train = self.predict(W, b, X_train)
+            y_pred_val = self.predict(W,b, X_val)
+            train_acc = sum(y_pred_train == Y_train) / len(Y_train)
+            val_acc = sum(y_pred_val == Y_val) / len(Y_val)
+
+            train_acc_list.append(train_acc)
+            val_acc_list.append(val_acc)
 
 
 
         train_costs = np.array(train_costs_list)
         val_costs = np.array(val_costs_list)
 
-        train_costs = sum(train_costs)/10
+        print ("train_costs", train_costs.shape)
+        train_costs = np.sum(train_costs,axis=0)/5
         print(train_costs)
-        val_costs = sum(val_costs)/10
-        print(val_costs)
+        print(train_costs.shape)
+        val_costs = np.sum(val_costs,axis=0)/5
+        #print(val_costs)
         W = params['W']
         b = params['b']
 
-        y_pred_train = self.predict(W,b,X_train)
+
+
+        train_acc = sum(train_acc_list) / len(train_acc_list)
+        val_acc = sum(val_acc_list) / len(val_acc_list)
+
         y_pred_test = self.predict(W,b,X_test)
-
-
-        train_acc = sum(y_pred_train == Y_train) / len(Y_train)
         test_acc = sum(y_pred_test == Y_test) / len(Y_test)
-        val_acc = sum()
+
 
         print ("Train acc:", train_acc)
+        print ("Val acc:", val_acc)
         print ("Test acc:", test_acc)
 
         return train_costs, val_costs
